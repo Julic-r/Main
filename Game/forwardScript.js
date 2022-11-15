f9fd3065ecdc6d71f7c48f13cadddccfe2763b84
Prof. Dr. Malte Weiß <weiss@weiyu-digital.com>
2022-11-15 18:31:44 +0100

    Game: Fixed html bug. Fixed URL.

--- Game/forwardScript.js
+++ Game/forwardScript.js
@@ -1,4 +1,4 @@
-var source = new EventSource('http://localhost:8080/button-click');
+var source = new EventSource('https://codingprof.hs-rw.de/cityrevivalbackend/button-click');
 source.addEventListener("message", (e) => {
 
     if(!(document.location.href == "http://127.0.0.1:8887/game.html")){
